48eac163961e40ab02bc6595b324ae856b120782
Devanshu Kumar Singh <console.uk123@gmail.com>
2025-03-09 04:20:19 +0530

    initial-commit3

--- config/corsoption.js
+++ config/corsoption.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ['http://localhost:3000','http://localhost:3001','http://localhost:3002', 'https://tourism-five-azure.vercel.app'], // Adjust this based on where your frontend is running
+  origin: ['http://localhost:3000','http://localhost:3001','http://localhost:3002','https://event-management-backend-gamma.vercel.app', 'https://tourism-five-azure.vercel.app'], // Adjust this based on where your frontend is running
   methods: ['GET', 'POST'],
   allowedHeaders: ['Authorization','Content-Type'],
   credentials: true,
